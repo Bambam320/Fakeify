df7344be31ad6bfee84c74827ea1d8a00e143ed0
Bambam320 <igor.mestechkin@gmail.com>
2022-12-28 14:23:19 -0500

    checking deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/hello', to: 'application#hello_world'
-#add routes here
+#add routes here and more
   get '*path',
   to: 'fallback#index',
   constraints: ->(req) { !req.xhr? && req.format.html? }
