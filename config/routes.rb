2f65215ff3b686460b40bd8100f7d5eb66c7605b
phase5-capstone <fakeprojectphase5@gmail.com>
2022-12-28 14:24:13 -0500

    checking deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/hello', to: 'application#hello_world'
-#add routes here and more
+#add routes here and more and more
   get '*path',
   to: 'fallback#index',
   constraints: ->(req) { !req.xhr? && req.format.html? }
