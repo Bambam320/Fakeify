829771c593962d1573493d1a4a1fd5ff1b437249
phase5-capstone <igor.mestechkin@gmail.com>
2023-05-25 01:17:02 -0400

    adding axios

--- client/src/Structure/App.js
+++ client/src/Structure/App.js
@@ -39,6 +39,17 @@ const App = () => {
   // logs them in, sets localuser, set login error for login component
   // , sets authenticated to allow routes
   
+  // async function fetchData() {
+  //   try {
+  //     const response = await axios.get('https://api.example.com/posts');
+  //     const data = response.data;
+  //     console.log(data);
+  //     // Work with the data here
+  //   } catch (error) {
+  //     console.log('Error:', error);
+  //   }
+  // }
+
   useEffect(() => {
     (async () => {
       const response = await fetch("/me");
